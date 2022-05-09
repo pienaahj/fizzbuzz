52f890c1444b5d6334583e4fbff8a78869f38a64
Hendrik Pienaar <hendrikpienaar@Henriettes-iMac.local>
2022-05-09 13:53:59 +0200

    WIP

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -9,6 +9,8 @@ func Run(ints []int) []string {
 	for _, i := range ints {
 		if i%3 == 0 {
 			strings = append(strings, "Fizz")
+		} else if i%5 == 0 {
+			strings = append(strings, "Buzz")
 		} else {
 			strings = append(strings, fmt.Sprint(i))
 		}
